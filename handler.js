d1b41ff2f5fbc5c713794bf478eea4cd83917d11
Valdes, Noriel <nvaldes@c21stores.com>
2017-06-02 11:55:00 -0400

    BROKEN: added cognito

--- handler.js
+++ handler.js
@@ -1,16 +1,40 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
+module.exports.signup = (event, context, callback) => {
+  var ret = null;
+  var req = JSON.parse(event.body);
+  var AWS = require('aws-sdk');
+  var userPool = new AWS.CognitoIdentityServiceProvider.CognitoUserPool({
+    UserPoolId: process.env.USER_POOL_ID,
+    ClientId: process.env.USER_POOL_CLIENT_ID
+  });
+  userPool.signUp(req.email, req.password, [
+    {
+      Name: 'email',
+      Value: req.email
+    },
+    {
+      Name: 'given_name',
+      Value: req.given_name
+    },
+    {
+      Name: 'family_name',
+      Value: req.family_name
+    }
+  ], (err, res) => {
+    if (err) {
+      ret = err;
+    } else {
+      ret = res;
+    }
+  });
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
+      message: 'User creation returned successfully!  Check result to see what happened.',
+      result: ret,
     }),
   };
 
   callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
