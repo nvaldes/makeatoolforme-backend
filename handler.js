552ff2f59e141e6ef5f93cd826be06b81e488d58
Valdes, Noriel <nvaldes@c21stores.com>
2017-06-20 17:32:22 -0400

    user creation works

--- handler.js
+++ handler.js
@@ -2,32 +2,33 @@
 
 module.exports.signup = (event, context, callback) => {
   var ret = null;
-  var req = JSON.parse(event.body);
-  var AWS = require('aws-sdk');
-  var userPool = new AWS.CognitoIdentityServiceProvider.CognitoUserPool({
+  var req = typeof event.body === 'string' ? JSON.parse(event.body) : event.body;
+  var AmazonCognitoIdentity = require('amazon-cognito-identity-js');
+  var userPool = new AmazonCognitoIdentity.CognitoUserPool({
     UserPoolId: process.env.USER_POOL_ID,
     ClientId: process.env.USER_POOL_CLIENT_ID
   });
-  userPool.signUp(req.email, req.password, [
+  var userAttributeList = [
     {
       Name: 'email',
       Value: req.email
-    },
-    {
-      Name: 'given_name',
-      Value: req.given_name
-    },
-    {
-      Name: 'family_name',
-      Value: req.family_name
     }
-  ], (err, res) => {
-    if (err) {
-      ret = err;
-    } else {
-      ret = res;
+  ];
+  userPool.signUp(
+    req.email,
+    req.password,
+    userAttributeList.map((e) => {
+      return new AmazonCognitoIdentity.CognitoUserAttribute(e);
+    }),
+    null,
+    (err, res) => {
+      if (err) {
+        ret = err;
+      } else {
+        ret = res;
+      }
     }
-  });
+  );
   const response = {
     statusCode: 200,
     body: JSON.stringify({
